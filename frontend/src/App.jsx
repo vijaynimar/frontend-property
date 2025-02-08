67898a632d81bdac19e0d0aacc2832ca47485365
Vijay Nimar <vijaynimar8@gmail.com>
2025-02-08 12:24:05 +0530

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Home from "./components/Home";
-import Parent from "./components/common/Parent";
-import Property from "./components/Property";
-
-const App = () => {
-  return <Parent />;
-};
-
-export default App;
